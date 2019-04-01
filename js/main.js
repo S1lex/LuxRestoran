522711d5ae9a47c5767e6c3e9737201c278f0054
Silex <popshupychka@gmail.com>
2019-04-01 23:34:23 +0300

    complete about section, add today special cards

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 window.onload = () => {
     let menuElem = document.getElementById('ulList');
     let titleElem = document.getElementById('title');
-    let btnDot = document.querySelectorAll("#btnDot");
     let btns = document.querySelectorAll('.do');
-    let imgs = ["url(./pictures/1.png)", "url(./pictures/sheet.jpg)", "url(./backgrounds/headerBanner.png)"];
+    let btnDot = document.querySelectorAll("#btnDot");
+    let imgs = ["url(./backgrounds/back1.png)", "url(./backgrounds/back2.png)", "url(./backgrounds/back3.png)"];
     let sliderWr = document.getElementsByClassName("slider-wr");
     
 
@@ -15,6 +15,7 @@ window.onload = () => {
         menuElem.classList.toggle("close");
     }
   });
+
   let changePhoto = (elem) => {
     if(elem.children[0].classList.contains("firstPhoto")) {
       sliderWr[0].style.backgroundImage = imgs[0];
@@ -23,7 +24,7 @@ window.onload = () => {
     } if (elem.children[0].classList.contains("thirdPhoto")) {
       sliderWr[0].style.backgroundImage = imgs[2]; 
     }
-  }
+  };
 
     let slider = () => {
       for(let i = 0; i < btns.length; i++) {
@@ -32,9 +33,9 @@ window.onload = () => {
             changePhoto(elem);
             dotTrack();
             elem.children[0].classList.toggle("dot");
-          }); 
+        });
     };
-  }
+  };
 
   let dotTrack = () => {
     btnDot.forEach((elem) => {
@@ -42,7 +43,27 @@ window.onload = () => {
     })
   }
     slider();
-
-  
 } 
 
+let btnToday = document.querySelectorAll(".btn-today");
+let dotToday = document.querySelectorAll("#dotToday");
+
+for(let i = 0; i < btnToday.length; i++) {
+  let item = btnToday[i];
+  item.addEventListener("click", () => {
+    dotTrack();
+    item.children[0].classList.toggle("dot");
+  })
+};
+
+let dotTrack = () => {
+  dotToday.forEach((elem) => {
+    elem.classList.remove("dot");
+  })
+};
+
+let cards = new Map([
+  ['img1', './srcblblblb'],
+  ['img2', 'blabalbbla']
+]);
+console.log(cards.get("img1"))
