53211d1959af5bfda66633da3467cb90dd9654af
Silex <popshupychka@gmail.com>
2019-04-06 11:33:31 +0300

    done today special

--- js/main.js
+++ js/main.js
@@ -48,13 +48,17 @@ window.onload = () => {
 let btnToday = document.querySelectorAll(".btn-today");
 let dotToday = document.querySelectorAll("#dotToday");
 
-for(let i = 0; i < btnToday.length; i++) {
-  let item = btnToday[i];
-  item.addEventListener("click", () => {
-    dotTrack();
-    item.children[0].classList.toggle("dot");
-  })
+let specialCards = () => {
+  for(let i = 0; i < btnToday.length; i++) {
+    let item = btnToday[i];
+    item.addEventListener("click", () => {
+      dotTrack();
+      cardChanger(item);
+      item.children[0].classList.toggle("dot");
+    })
+  }
 };
+
 
 let dotTrack = () => {
   dotToday.forEach((elem) => {
@@ -62,8 +66,102 @@ let dotTrack = () => {
   })
 };
 
-let cards = new Map([
-  ['img1', './srcblblblb'],
-  ['img2', 'blabalbbla']
-]);
-console.log(cards.get("img1"))
+let cardChanger = (item) => {
+  if(item.children[0].classList.contains("firstPhoto")) {
+    arrayCard["first"][4][0]();
+    arrayCard["second"][4][0]();
+    console.log(arrayCard["first"][4][0]);
+  } if (item.children[0].classList.contains("secondPhoto")) {
+    arrayCard["third"][4][0]();
+    arrayCard["fourth"][4][0]();
+  } if (item.children[0].classList.contains("thirdPhoto")) {
+    arrayCard["fifth"][4][0]();
+    arrayCard["sixth"][4][0]();
+  }
+}
+
+let cardNameFirst = document.getElementById("cardNameFirst");
+let cardNameSecond = document.getElementById("cardNameSecond");
+let imgCardsFirst = document.getElementById("cardImgFirst");
+let imgCardsSecond = document.getElementById("cardImgSecond");
+let infoCardsFirts = document.getElementById("cardInfoFirst");
+let infoCardsSecond = document.getElementById("cardInfoSecond");
+let priceCardsFirst = document.getElementById("cardPriceFirst");
+let priceCardsSecond = document.getElementById("cardPriceSecond");
+
+let arrayCard = {
+  "first": [
+      [imgCardsFirst, "./backgrounds/back1.png"],
+      [cardNameFirst, "First food"],
+      [infoCardsFirts, "emLoremLoremLoremLoremLoremLoremLoremLoremLorem iLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem iLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem"],
+      [priceCardsFirst, "<sup>$</sup>81"],
+      [() => {
+        arrayCard["first"][0][0].src = arrayCard["first"][0][1];
+        arrayCard["first"][1][0].innerHTML = arrayCard["first"][1][1];
+        arrayCard["first"][2][0].innerText = arrayCard["first"][2][1];
+        arrayCard["first"][3][0].innerHTML = arrayCard["first"][3][1];
+        console.log("yeah!");
+      }]
+  ],
+  "second": [
+      [imgCardsSecond, "./backgrounds/back2.png"],
+      [cardNameSecond, "Second food"],
+      [infoCardsSecond, "asdasfldkgjfdlkjhgfdlk sjlfdks;jhg iLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem ldksfjh l;kdfsj l;dkfsjhl;kdfsjhl;ksdfjhl;kjdhpadh"],
+      [priceCardsSecond, "<sup>$</sup>92.00"],
+      [() => {
+        arrayCard["second"][0][0].src = arrayCard["second"][0][1];
+        arrayCard["second"][1][0].innerHTML = arrayCard["second"][1][1];
+        arrayCard["second"][2][0].innerText = arrayCard["second"][2][1];
+        arrayCard["first"][3][0].innerHTML = arrayCard["second"][3][1]; 
+      }]
+  ],
+  "third": [
+    [imgCardsFirst, "./backgrounds/back2.png"],
+    [cardNameFirst, "third food"],
+    [infoCardsFirts, "asdasfldkgjfdlkjhgfdlk sjlfdks;jhg iLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem ldksfjh l;kdfsj l;dkfsjhl;kdfsjhl;ksdfjhl;kjdhpadh"],
+    [priceCardsFirst, "<sup>$</sup>93.00"],
+    [() => {
+      arrayCard["third"][0][0].src = arrayCard["third"][0][1];
+      arrayCard["third"][1][0].innerHTML = arrayCard["third"][1][1];
+      arrayCard["third"][2][0].innerText = arrayCard["third"][2][1];
+      arrayCard["third"][3][0].innerHTML = arrayCard["third"][3][1]; 
+    }]
+  ],
+  "fourth": [
+    [imgCardsSecond, "./backgrounds/back2.png"],
+    [cardNameSecond, "fourth food"],
+    [infoCardsSecond, "asdasfldkgjfdlkjhgfdlk sjlfdks;jhg iLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem ldksfjh l;kdfsj l;dkfsjhl;kdfsjhl;ksdfjhl;kjdhpadh"],
+    [priceCardsSecond, "<sup>$</sup>94.00"],
+    [() => {
+      arrayCard["fourth"][0][0].src = arrayCard["fourth"][0][1];
+      arrayCard["fourth"][1][0].innerHTML = arrayCard["fourth"][1][1];
+      arrayCard["fourth"][2][0].innerText = arrayCard["fourth"][2][1];
+      arrayCard["fourth"][3][0].innerHTML = arrayCard["fourth"][3][1]; 
+    }]
+  ],
+  "fifth": [
+    [imgCardsFirst, "./backgrounds/back2.png"],
+    [cardNameFirst, "fifth food"],
+    [infoCardsFirts, "asdasfldkgjfdlkjhgfdlk sjlfdks;jhg iLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem ldksfjh l;kdfsj l;dkfsjhl;kdfsjhl;ksdfjhl;kjdhpadh"],
+    [priceCardsFirst, "<sup>$</sup>95.00"],
+    [() => {
+      arrayCard["fifth"][0][0].src = arrayCard["fifth"][0][1];
+      arrayCard["fifth"][1][0].innerHTML = arrayCard["fifth"][1][1];
+      arrayCard["fifth"][2][0].innerText = arrayCard["fifth"][2][1];
+      arrayCard["fifth"][3][0].innerHTML = arrayCard["fifth"][3][1]; 
+    }]
+  ],
+  "sixth": [
+    [imgCardsSecond, "./backgrounds/back2.png"],
+    [cardNameSecond, "sixth food"],
+    [infoCardsSecond, "asdasfldkgjfdlkjhgfdlk sjlfdks;jhg iLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem ldksfjh l;kdfsj l;dkfsjhl;kdfsjhl;ksdfjhl;kjdhpadh"],
+    [priceCardsSecond, "<sup>$</sup>96.00"],
+    [() => {
+      arrayCard["sixth"][0][0].src = arrayCard["sixth"][0][1];
+      arrayCard["sixth"][1][0].innerHTML = arrayCard["sixth"][1][1];
+      arrayCard["sixth"][2][0].innerText = arrayCard["sixth"][2][1];
+      arrayCard["sixth"][3][0].innerHTML = arrayCard["sixth"][3][1]; 
+    }]
+  ]
+  };
+specialCards();
